23ab302ceb6e3cef0b4b26455a472c374436af52
mateoroldos <mateoroldos19@gmail.com>
2023-04-03 15:13:09 -0300

    feat: Project basic structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="primary">
+  <h1 class="display-small on-primary-text">Welcome to SvelteKit</h1>
+  <p class="body-medium on-primary-text">
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</section>
