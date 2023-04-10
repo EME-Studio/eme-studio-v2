fd628a0ea0225f8470ead8f5979b35a1d76c409a
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-04-10 16:23:43 -0300

    basic scss structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   import "../app.postcss";
   import "../app.scss";
-  import type { LayoutData } from "./$types";
+  // import "../fonts/SpaceGrotesk-VariableFont_wght.ttf";
+  // import type { LayoutData } from "./$types";
 
-  export let data: LayoutData;
+  // export let data: LayoutData;
 </script>
 
 <slot />
+
+<style global>
+  body {
+    font-family: "Space Grotesk";
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
-<section class="primary">
-  <h1 class="display-small on-primary-text">Welcome to SvelteKit</h1>
-  <p class="body-medium on-primary-text">
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-  </p>
-</section>
+<div class="section">
+  <div class="container">
+    <h1 class="display-large">Welcome to SvelteKit</h1>
+    <p class="body-medium on-primary-text">
+      Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+  </div>
+</div>
+
+<style type="scss">
+</style>
