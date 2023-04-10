38362f805efd2779c6072998c90fc6aa505f6d8c
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-04-10 17:55:31 -0300

    scss base minor corrections

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,6 @@
 <script lang="ts">
   import "../app.postcss";
   import "../app.scss";
-  // import "../fonts/SpaceGrotesk-VariableFont_wght.ttf";
-  // import type { LayoutData } from "./$types";
-
-  // export let data: LayoutData;
 </script>
 
 <slot />
-
-<style global>
-  body {
-    font-family: "Space Grotesk";
-  }
-</style>
