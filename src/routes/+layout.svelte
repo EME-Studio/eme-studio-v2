a036371c9d2f6cbbbcf0643c41a38f06968b302d
Mateo Roldos <mateoroldos19@gmail.com>
2023-04-18 13:04:20 -0300

    Revert "Ape/eme 218 layout components"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
-  import Header from "$lib/components/Header.svelte";
-  import Footer from "$lib/components/Footer.svelte";
   import "../app.postcss";
   import "../app.scss";
 </script>
 
-<Header />
-<main>
-  <slot />
-</main>
-<Footer />
+<slot />
