77945bae9e632361526969ad946e1df4d6e29d32
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-04-25 15:15:48 -0300

    Npm install, svelte adapter updated in svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
 import { mdsvex } from "mdsvex";
 import sveltePreprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -22,10 +23,13 @@ const config = {
   ],
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      // See below for an explanation of these options
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"],
+      },
+    }),
   },
 };
 
